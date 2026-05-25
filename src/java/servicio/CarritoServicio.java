fb8eee9e07c4580a00a78ac42b589f0b00d01f30
espinozajosue707-netizen <espinozajosue707@gmail.com>
2026-05-24 23:07:19 -0500

    Update CarritoServicio.java

--- src/java/servicio/CarritoServicio.java
+++ src/java/servicio/CarritoServicio.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package servicio;
-
-/**
- *
- * @author User
- */
+import java.util.ArrayList;
 public class CarritoServicio {
-    
+       private ArrayList<Producto> productos;
+
+    public Carrito() {
+
+        productos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+
+        productos.add(producto);
+    }
+
+    public ArrayList<Producto> getProductos() {
+
+        return productos;
 }
