78de43514a2dcc323e80863b53e3662941582520
Rojadeveloper <rojanodas@gmail.com>
2026-05-27 03:27:49 -0500

    refactor(dto): rename UsuarioDTO to LoginDTO for authentication clarity

--- src/java/modelo/dto/LoginDTO.java
+++ src/java/modelo/dto/LoginDTO.java
@@ -9,5 +9,30 @@ package modelo.dto;
  * @author User
  */
 public class LoginDTO {
+    private String nombre;
+    private String correo;
     
+    public LoginDTO() {
+    }
+
+    public LoginDTO(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
